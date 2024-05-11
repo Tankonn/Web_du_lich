a738f79fa973bbdd815e27577259ed97f0500759
Tankonn <ttpro115@gmail.com>
2024-05-11 19:33:44 +0700

    Add project files.

---
+++ do_an_co_so/Controllers/searchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace do_an_co_so.Controllers
+{
+    public class searchController : Controller
+    {
+        public IActionResult search()
+        {
+            return View();
+        }
+    }
+}
