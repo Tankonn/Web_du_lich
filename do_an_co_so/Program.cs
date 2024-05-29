5f99be1a92900ce79a2a0a640d3d59b456afb890
Minh9203 <huynhnhatminh0902@gmail.com>
2024-05-29 19:38:04 +0700

    thêm service programs

--- do_an_co_so/Program.cs
+++ do_an_co_so/Program.cs
@@ -1,12 +1,19 @@
 using do_an_co_so.DataAccess;
+using do_an_co_so.Repositories;
 using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Core.Types;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
+builder.Services.AddRazorPages();
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddScoped<IblogRepository, EFblogRepository>();
+builder.Services.AddScoped<IdattourRepository, EFdattourRepository>();
+builder.Services.AddScoped<IkhachsanRepository, EFkhachsanRepository>();
+builder.Services.AddScoped<IphuongthucthanhtoanRepository, EFphuongthucthanhtoanRepository>();
+builder.Services.AddScoped<ItourRepository, EFtourRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
